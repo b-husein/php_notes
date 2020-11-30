bb2a4402dba71853618caf44f0e8ee769d5eb658
b-husein <bajrektarevic.husein@gmail.com>
2020-11-30 11:35:07 +0100

    initial commit

---
+++ 00_intro.php
@@ -0,0 +1,23 @@
+<?php 
+
+    // Basic principles of PHP
+    // PHP7 / Apache / MySQL Setup (XAMPP server)
+    // Code syntax (vars, arrays, loops, conditionals, functions, etc.)
+    // Procedural & OOP 
+    // Dynamic Pages 
+    // Form Submission / Superglobals
+    // Working with Databases
+    // Mini projects (Contact Form, User Login System, etc.)
+
+    
+
+
+
+
+
+
+
+
+
+
+?>
