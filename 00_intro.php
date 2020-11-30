405851ae8e11e324c74f4d31b00eb271b70e1a8f
b-husein <bajrektarevic.husein@gmail.com>
2020-11-30 11:40:33 +0100

    updated info

--- 00_intro.php
+++ 00_intro.php
@@ -9,7 +9,31 @@
     // Working with Databases
     // Mini projects (Contact Form, User Login System, etc.)
 
+    /* 
+
+    What is PHP?
     
+    PHP: Hypertext Preprocessor (Recursive acronym);
+
+    Server Side Programming / Scripting Language
+
+    Can be embedded directly within HTML (<?php ?>)
+    
+    Files use a ".php" file extension
+
+    How Does PHP Work? 
+
+    Client makes a request 
+
+    Server runs PHP / extensions 
+
+    Query database if needed (one of the best PHP features)
+
+    Load other files if needed
+
+    Result is sent back to client (or browser)
+    
+    */
 
 
 
