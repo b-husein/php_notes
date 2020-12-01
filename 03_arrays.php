d9d396570e0b9beb52476d8986712a6f02d24ae4
b-husein <bajrektarevic.husein@gmail.com>
2020-12-01 09:20:38 +0100

    updated

--- 03_arrays.php
+++ 03_arrays.php
@@ -4,13 +4,13 @@
     /* 
         Three types of arrays: 
 
-        - Indexed
-        - Associative
-        - Multi-dimensional
+        1) Indexed
+        2) Associative
+        3) Multi-dimensional
     
     */
 
-    // INDEXED ARRAYS
+    // 1) INDEXED ARRAYS
 
     $people = array('Kevin', 'Jeremy', 'Sara');
     // to access one of these using index values;
@@ -25,6 +25,81 @@
     // and this is a more convenient way;
 
     $cars = ['Honda', 'Toyota', 'Ford'];
-    // echo $cars[1];
+    // add item to array:
+    $cars[3] = 'Chevy';
+    // add item without using index value:
+    $cars[] = 'Mustang';
+    // echo $cars[4];
+
+    // FUNCTIONS with Arrays: 
+
+    # count function; 
+    // for example, we want to know how many cars
+    // we have had in previous array:
+
+    // echo count($cars);
+
+    # print_r function; 
+    // which is basically an echo for array
+    // it will show us the whole array; 
+
+    // print_r($cars);
+
+    # var_dump();
+    // very popular debugging function
+    // we can see what kind of data types we have; 
+
+    // var_dump($cars);
+
+    // -------------------------------------------------------------
+    
+    // 2) ASSOCIATIVE ARRAYS
+
+    // it allows us to define a key and a value; 
+    // $arrayName = array('key' => 'value'); 
+
+    $people = array('Brad' => 35, 'Jose' => 32, 'William' => 37); 
+    // string is a key
+    // number is a value
+
+    // to access associative arrays values
+    // we will write the following syntax
+    // echo $arrayName['key']; 
+    // as output we will get a number value from the array.
+
+    // echo $people['Brad'];
+    
+    // now we will create another array named ids; 
+    // we will use another syntax with square brackets
+    // and we will set number values to strings
+
+    $ids = [22 => 'Brad', 44 => 'Jose', 63 => 'William'];
+    // echo $ids[22];
+
+    // if we want to add to an array: 
+    $people['Jill'] = 42; 
+    // echo $people['Jill']; 
+
+    // print_r($people);
+
+    // -------------------------------------------------------------
+
+    // 3) MULTIDIMENSIONAL ARRAYS:
+
+    // it's an array within an array; 
+
+    $cars = array(
+        array('Honda', 20, 10),
+        array('Toyota', 30, 20),
+        array('Ford', 23, 12)
+    ); 
+
+    // when we echo this out, we will write name of array
+    // with two sets of brackets
+    // first one will be which index value we want to echo out
+    // the second one is the name; 
+
+    // echo $cars[1][2];
+
 
 ?>
