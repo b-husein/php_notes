f3c84957d85de2a3e6d366f3ad9b490bb8deea80
b-husein <bajrektarevic.husein@gmail.com>
2020-12-01 08:39:42 +0100

    initial commit

---
+++ 03_arrays.php
@@ -0,0 +1,17 @@
+<?php
+    # Array - variable that can hold multiple values. 
+
+    /* 
+        Three types of arrays: 
+
+        - Indexed
+        - Associative
+        - Multi-dimensional
+    
+    */
+
+    // INDEXED ARRAYS
+
+    
+
+?>
