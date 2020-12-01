f9493187980e9689ca2980b1dbb313d090adf295
b-husein <bajrektarevic.husein@gmail.com>
2020-12-01 08:50:54 +0100

    updated

--- 03_arrays.php
+++ 03_arrays.php
@@ -12,6 +12,19 @@
 
     // INDEXED ARRAYS
 
-    
+    $people = array('Kevin', 'Jeremy', 'Sara');
+    // to access one of these using index values;
+    // have in mind that arrays are zero based.
+    // echo $people[1];
+
+    $ids = array(23, 55, 12);
+    // echo $ids[2];
+
+    # Another way to write arrays, without using array function;
+    // but it's equivalent to using array function; 
+    // and this is a more convenient way;
+
+    $cars = ['Honda', 'Toyota', 'Ford'];
+    // echo $cars[1];
 
 ?>
