6a1d04c5c427e961cfa7e5f99577d1b1f661ed57
b-husein <bajrektarevic.husein@gmail.com>
2020-12-02 19:46:07 +0100

    initial commit

---
+++ 07_dates_timestamps.php
@@ -0,0 +1,32 @@
+<?php 
+
+    # DATES
+
+    // echo date('d'); // day
+    // echo date('m'); // month
+    // echo date('y'); // year
+    // echo date('l'); // day of the week
+    // echo date('m-d-Y'); // month-day-year
+
+    # TIME
+
+    // echo date('h'); // Hour
+    // echo date('i'); // min
+    // echo date('s'); // sec
+    // echo date('a'); // min
+    // echo date('h:i:sa'); // hour-min-sec
+
+    // Set time zone
+    // date_default_timezone_set('America/New_York'); 
+
+
+    
+
+
+    
+
+
+
+
+
+?>
