146d4153c36ade6be60cc71bfc39ac747a51e407
b-husein <bajrektarevic.husein@gmail.com>
2020-12-02 20:14:49 +0100

    initial commit

---
+++ 08_include_require/website_01/index.php
@@ -0,0 +1,13 @@
+<?php include 'inc/header.php'; ?>
+
+        <h1>Home</h1>
+
+<!-- 
+Include and require statements allow us to insert 
+php code, markup, text into another php file.
+-->
+
+
+<?php include 'inc/footer.php'; ?>
+
+        
