79d85bc64b78dc4b3ede410e6e5cb9f9c4f3033d
b-husein <bajrektarevic.husein@gmail.com>
2020-12-03 18:23:17 +0100

    updated

--- 10_get_post.php
+++ 10_get_post.php
@@ -1,13 +1,16 @@
 <?php 
+    
     if(isset($_GET['name'])){
         // print_r($_GET);
-        // echo $_GET['name'];
+        echo $_GET['name'];
         // in order to prevent XSS attacks we will use htmlentities() function.
         // it will put malicious code <script>alert(1)</script>
         // into html entities &lt;script&gt;alert(1)&lt;/script&gt;
         $name = htmlentities($_POST['name']);
-        echo($name);
+        // echo($name);
     }
+
+    /* 
 
         // we will use POST if we want to send data and at the same time the data will not be visible in the URL;
         // much more secure way to send data; 
@@ -17,8 +20,29 @@
         print_r($_POST);
         $name = htmlentities($_POST['name']);
     }
-
+    
         // the third way to get this data is by using request;
+        // this will work regardless of the value
+        // inside the form (POST or GET)
+        // and this is only an alternative way of doing it.
+
+    if(isset($_REQUEST['name'])){
+        // print_r($_REQUEST);
+        $name = htmlentities($_REQUEST['name']);
+        echo $name;
+    }
+    
+    echo $_SERVER['QUERY_STRING'];
+    */
+
+    // another thing we need to mention is that we can send query strings
+    // in the URL itself without using a form
+
+    
+
+    
+
+        
 ?>
 
 <!DOCTYPE html>
@@ -29,7 +53,7 @@
     <title>My Website</title>
 </head>
 <body>
-    <form method="POST" action="10_get_post.php">
+    <form method="GET" action="10_get_post.php">
         <div>
             <label>Name: </label><br>
             <input type="text" name="name">
@@ -40,5 +64,20 @@
         </div>
         <input type="submit" value="Submit">
     </form>
+
+    <ul>
+        <li>
+            <a href="10_get_post.php?name=Husein">Husein</a>
+        </li>
+        <li>
+            <a href="10_get_post.php?name=Steve">Steve</a>
+        </li>
+        <li>
+            <a href="10_get_post.php?name=Lucy">Lucy</a>
+        </li>
+    </ul>
+
+    <h1><?php echo "{$name}'s Profile"; ?></h1>
+
 </body>
 </html>
